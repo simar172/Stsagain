f9575aba00320d7b1601f649e7f780752ed1a5a2
simar172 <109204392+simar172@users.noreply.github.com>
2022-10-16 13:09:52 +0530

    Delete ytproj directory

--- ytproj/src/main/java/com/example/ytproj/config/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.example.ytproj.config;
-
-public class Constants {
-    public static final String PageNo = "0";
-    public static final String PageSize = "10";
-    public static final String sortBy = "id";
-    public static final String sortDir = "asc";
-}
